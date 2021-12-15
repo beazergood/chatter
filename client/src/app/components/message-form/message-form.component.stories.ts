cd0cb7fa989b29556ce013f35ad96c9e8d503c3d
Dave Beazer <dave@sideprojectco.com>
2021-12-15 23:39:02 +0000

    better UX, more stories, interactive stories working

--- client/src/app/components/message-form/message-form.component.stories.ts
+++ client/src/app/components/message-form/message-form.component.stories.ts
@@ -2,15 +2,18 @@ import { action } from '@storybook/addon-actions'
 import { Story, Meta, moduleMetadata } from '@storybook/angular'
 import { expect } from '@storybook/jest'
 import { userEvent, waitFor, within } from '@storybook/testing-library'
-import MessageFormComponent from './message-form.component'
 import { ReactiveFormsModule, FormBuilder } from '@angular/forms'
+import { CommonModule } from '@angular/common'
+
+// component under test
+import MessageFormComponent from './message-form.component'
 
 export default {
   title: 'Components/MessageForm',
   component: MessageFormComponent,
   decorators: [
     moduleMetadata({
-      imports: [ReactiveFormsModule],
+      imports: [CommonModule, ReactiveFormsModule],
       providers: [FormBuilder],
     }),
   ],
@@ -23,35 +26,58 @@ export const actionsData = {
 
 const Template: Story<MessageFormComponent> = (args: MessageFormComponent) => ({
   props: {
-    ...args,
-    onSendMessage: actionsData.onSendMessage,
+    argTypes: {
+      onSendMessage: { action: 'clicked' },
+    },
   },
 })
 // Default scenario
 export const EmptyMessageForm = Template.bind({})
 EmptyMessageForm.args = {}
 
-export const ValidMessageForm = Template.bind({})
-ValidMessageForm.args = {}
-ValidMessageForm.play = async ({ args, canvasElement }) => {
+export const NameEntered = Template.bind({})
+NameEntered.play = async ({ args, canvasElement }) => {
   const canvas = within(canvasElement)
+  await userEvent.type(canvas.getByTestId('name'), 'Le Bot')
+}
 
+export const ValidForm = Template.bind({})
+ValidForm.play = async ({ args, canvasElement }) => {
+  const canvas = within(canvasElement)
   await userEvent.type(canvas.getByTestId('name'), 'Le Bot')
   await userEvent.type(canvas.getByTestId('message'), 'Bonjour')
 }
 
-export const CompletedMessageForm = Template.bind({})
-CompletedMessageForm.args = {
-  // name: 'Dave',
-  // message: 'Hello world',
-  // freezeName: true,
-}
-CompletedMessageForm.play = async ({ args, canvasElement }) => {
+export const SubmittedForm = Template.bind({})
+SubmittedForm.play = async ({ args, canvasElement }) => {
   const canvas = within(canvasElement)
-
-  await userEvent.type(canvas.getByTestId('name'), 'Le Bot')
-  await userEvent.type(canvas.getByTestId('message'), 'Bonjour')
+  await userEvent.type(canvas.getByTestId('name'), 'Santa Paws', { delay: 50 })
+  await userEvent.type(canvas.getByTestId('message'), 'Bonjour', {
+    delay: 50,
+  })
   await userEvent.click(canvas.getByTestId('submit-button'))
+  // await waitFor(() => expect(args.onSendMessage).toHaveBeenCalled()) // why doesn't this work?
+}
 
-  await waitFor(() => expect(args.sendMessage).toHaveBeenCalled())
+export const SubmittedAndNameEdited = Template.bind({})
+SubmittedAndNameEdited.play = async ({ args, canvasElement }) => {
+  const canvas = within(canvasElement)
+  await userEvent.type(canvas.getByTestId('name'), 'Santa Paws', { delay: 50 })
+  await userEvent.type(canvas.getByTestId('message'), 'Bonjour', {
+    delay: 50,
+  })
+  await userEvent.click(canvas.getByTestId('submit-button'))
+  await userEvent.click(canvas.getByTestId('name-edit-button'))
+  await userEvent.type(
+    canvas.getByTestId('name'),
+    ' {backspace}{backspace}{backspace}{backspace}{backspace}{backspace} Claws',
+    {
+      delay: 50,
+    }
+  )
+  await userEvent.type(canvas.getByTestId('message'), 'Bonjour', {
+    delay: 50,
+  })
+
+  // await waitFor(() => expect(args.onSendMessage).toHaveBeenCalled()) // why not working?!
 }
--- client/src/app/components/message-form/message-form.component.ts
+++ client/src/app/components/message-form/message-form.component.ts
@@ -1,5 +1,12 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core'
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  OnDestroy,
+  Output,
+} from '@angular/core'
 import { FormBuilder, FormGroup, Validators } from '@angular/forms'
+import { debounceTime, filter, map, Subscription, tap } from 'rxjs'
 
 @Component({
   selector: 'app-message-form',
@@ -7,15 +14,22 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms'
     <div class="flex flex-row mt-10">
       <div class="bg-gray-200 shadow-md rounded px-8 pt-6 pb-8 mb-4 flex-1">
         <form [formGroup]="messageForm" autocomplete="off">
-          <fieldset
+          <button
             *ngIf="hideNameInput"
-            class="mb-2"
+            data-testid="name-edit-button"
+            class="border my-3 rounded-md text-xs inline-block  text-gray-400 px-2 py-1"
             (click)="hideNameInput = !hideNameInput"
           >
-            <label>{{ messageForm.controls.name.value }}</label>
+            Your name (click to edit)
+          </button>
+          <fieldset
+            *ngIf="hideNameInput"
+            class="mb-2 cursor-pointer inline-block"
+          >
+            <label class="mr-3">{{ messageForm.controls.name.value }}</label>
           </fieldset>
           <fieldset *ngIf="!hideNameInput">
-            <div class="mb-4">
+            <div class="mb-4 mt-2">
               <input
                 class="shadow appearance-none rounded w-full py-2 px-3 border
              text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
@@ -24,10 +38,11 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms'
                 type="text"
                 placeholder="Your name"
                 formControlName="name"
+                autofocus
               />
             </div>
           </fieldset>
-          <fieldset *ngIf="messageForm.controls.name.value">
+          <fieldset *ngIf="!hideMsgInput">
             <div class="mb-4">
               <input
                 class="shadow appearance-none rounded w-full py-2 px-3 border
@@ -59,28 +74,47 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms'
     </div>
   `,
 })
-export default class MessageFormComponent {
+export default class MessageFormComponent implements OnDestroy {
   @Output() onSendMessage: EventEmitter<any> = new EventEmitter()
 
   name: string = ''
   message: string = ''
   hideNameInput: boolean = false
-
-  constructor(private fb: FormBuilder) {}
+  hideMsgInput: boolean = true
+  formSubscription: Subscription
 
   messageForm: FormGroup = this.fb.group({
     name: [this.name, Validators.required],
     message: [this.message, Validators.required],
   })
 
+  constructor(private fb: FormBuilder, private el: ElementRef) {
+    this.formSubscription = this.messageForm.valueChanges
+      .pipe(
+        filter((formData) => !!formData),
+        // debounceTime(300),
+        map((formData) => {
+          this.hideMsgInput = formData.name ? false : true
+        })
+      )
+      .subscribe()
+  }
+
   submit(): void {
-    console.log('submitting form : ', this.messageForm.value)
     this.onSendMessage.emit(this.messageForm.value)
     this.afterSubmit()
   }
 
   private afterSubmit(): void {
-    this.hideNameInput = true // switches name input to a label
+    this.hideNameInput = true // switches name input for a label
     this.messageForm.controls.message.patchValue('') // resets the message input ready for next message
+    const invalidControl = this.el.nativeElement.querySelector(
+      '[formControlName="message"]'
+    )
+    invalidControl.focus() // focus message field for quicker chat!
+  }
+
+  ngOnDestroy(): void {
+    this.formSubscription.unsubscribe()
   }
 }
